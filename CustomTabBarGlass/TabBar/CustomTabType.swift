121e218090fc1e6bc83ac4dd16e2e77721df983d
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2026-01-09 21:33:27 +0200

    added CustomTabType

---
+++ CustomTabBarGlass/TabBar/CustomTabType.swift
@@ -0,0 +1,30 @@
+//
+
+
+import SwiftUI
+
+enum CustomTabType: String, CaseIterable {
+
+    case home = "Home"
+    case notifications = "Notifications"
+    case settings = "Settings"
+    
+    var symbol: String {
+        switch self {
+            case .home: return "house"
+            case .notifications: return "bell"
+            case .settings: return "gearshape"
+        }
+    }
+    var actionSymbol: String {
+        switch self {
+            case .home: return "plus"
+            case .notifications: return "tray.full.fill"
+            case .settings: return "cloud.moon.fill"
+        }
+    }
+    
+    var index: Int {
+        Self.allCases.firstIndex(of: self) ?? 0
+    }
+}
